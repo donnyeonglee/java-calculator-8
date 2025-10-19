2779cbf75ac8b1b507b206fb7b6c5e0b7dca4706
donnyeonglee <dlehssud@snu.ac.kr>
2025-10-19 21:23:57 +0900

    feat: 문자열 입력 기능 추가 - camp.nextstep.edu.missionutils.Console의 readLine()을 활용해 사용자로부터 문자열 입력 받는 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,9 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // Input 테스트
-        String input;
-        input = Console.readLine();
-        System.out.println(input);
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String rawInputString; //
+        rawInputString = Console.readLine();
+        //System.out.println("입력한 문자열 : " + rawInputString); // 테스트 출력
     }
 }
